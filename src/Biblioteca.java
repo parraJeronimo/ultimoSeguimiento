447eb385d982767356ffb1fae509c6e914e7ec02
Jeronimo Parra Hurtado <parra.jeronimo8380@eam.edu.co>
2026-03-02 17:17:56 -0500

    se crea un registrar cliente y un buscar cliente en biblioteca

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -4,9 +4,25 @@ public class Biblioteca {
     private String name;
     private Inventario inventario;
     private ArrayList<Cliente> clientes;
-    public Biblioteca(String name){
-        this.name=name;
-        this.inventario= new Inventario();
-        this.clientes= new ArrayList<>();
+
+    public Biblioteca(String name) {
+        this.name = name;
+        this.inventario = new Inventario();
+        this.clientes = new ArrayList<>();
     }
+
+    public void registrarCliente(Cliente cliente) {
+        clientes.add(cliente);
+    }
+
+    public Cliente buscarCliente(String id) {
+        for (Cliente c : clientes) {
+            if (c.getId().equals(id)) {
+                return c;
+            }
+        }
+        return null;
+    }
+
 }
+
